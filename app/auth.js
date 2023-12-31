d35618c83e174a8490e3797d96f6b2af6decbf23
Akshay Lilani <akshaylilani60@gmail.com>
2023-12-31 15:39:40 +0530

    🐛 FIX: Remove redundant code

--- app/auth.js
+++ app/auth.js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth"
-import credentials from "next-auth/providers/credentials"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { authConfig } from "./authconfig"
 import { connectDb } from "./lib/utils"

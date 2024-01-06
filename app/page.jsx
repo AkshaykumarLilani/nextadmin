3246f7f5dd2a80780740b29e2c38a1e15bb79763
Akshay Lilani <akshaylilani60@gmail.com>
2024-01-06 21:43:41 +0530

    👌 IMPROVE: Login Page UI

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,23 @@ import Link from "next/link"
 
 const Homepage = () => {
   return (
-    <div>
+    <div className="login-button-container">
       <Link href={'/login'}>
         Please Login
       </Link>
+      <div className="demo-account">
+        <h5>Demo Account</h5>
+        <table>
+          <tr>
+            <td>Username:</td>
+            <td>username</td>
+          </tr>
+          <tr>
+            <td>Password:</td>
+            <td>password</td>
+          </tr>
+        </table>
+      </div>
     </div>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.resolve.fallback = {
+            "mongodb-client-encryption": false ,
+            "aws4": false
+        }
+        return config;
+    },
     images: {
         remotePatterns: [
             {
